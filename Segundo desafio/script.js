ea518a34168f6076503b160945af0cf5f25d4752
Gabriela Donato <146032843+GabsDonato@users.noreply.github.com>
2024-09-06 09:54:06 -0300

    teste 1

--- Segundo desafio/script.js
+++ Segundo desafio/script.js
@@ -1,7 +1,11 @@
-ranking(50 - 5)
+ranking(80, 8)
 
-let jogador = ranking
+
 function ranking(vitorias, derrotas){
- 
-    
+    var jogador = ranking
+    var saldoDeRankeadas = (vitorias - derrotas)
 }
+
+
+
+console.log("O Herói tem de saldo de" + ranking + "está no nível de" + saldoDeRankeadas)
